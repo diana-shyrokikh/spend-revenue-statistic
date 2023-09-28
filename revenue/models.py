9cb9ef998b000727b48953ab8db014063413af61
Diana Shyrokikh <diashiroks@gmail.com>
2023-09-28 10:54:41 +0300

    :sparkles: Added revenue model in models.py

--- revenue/models.py
+++ revenue/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class RevenueStatistic(models.Model):
+    name = models.CharField(max_length=255)
+    spend = models.ForeignKey(
+        to="spend.Statistic",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    date = models.DateField()
+    revenue = models.DecimalField(
+        max_digits=9,
+        decimal_places=2,
+        default=0
+    )
