22bd925cdab658c7c74034fefd97e7b9899a7720
Diana Shyrokikh <diashiroks@gmail.com>
2023-09-28 17:22:52 +0300

    :sparkles: Added documentation urls

--- statistic_service/urls.py
+++ statistic_service/urls.py
@@ -1,9 +1,16 @@
 """
 URL configuration for statistic_service project.
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
 
 from revenue.views import RevenueViewSet
 from spend.views import SpendViewSet
@@ -13,7 +20,28 @@ router.register("spend", SpendViewSet)
 router.register("revenue", RevenueViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+
+    path(
+        "api/v1/doc/",
+        SpectacularAPIView.as_view(),
+        name="schema"
+    ),
+    path(
+        "api/v1/doc/swagger/",
+        SpectacularSwaggerView.as_view(
+            url_name="schema"
+        ),
+        name="swagger-ui"
+    ),
+    path(
+        "api/v1/doc/redoc/",
+        SpectacularRedocView.as_view(
+            url_name="schema"
+        ),
+        name="redoc"
+    ),
+
     path("api/v1/", include(router.urls)),
 
     path("api/v1/revenue-statistic/", include(
@@ -22,4 +50,4 @@ urlpatterns = [
     path("api/v1/spend-statistic/", include(
         "spend.urls", namespace="spend_app"
     )),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
